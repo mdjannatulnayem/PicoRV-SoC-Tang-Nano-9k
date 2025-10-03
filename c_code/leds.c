073c9ae56894cf0e59a522ed5411d098cb3e69a4
Md. Jannatul Nayem <nayemalimran106@gmail.com>
2025-10-04 00:13:25 +0600

    peripheral drivers

---
+++ c_code/leds.c
@@ -0,0 +1,14 @@
+
+#include "leds.h"
+
+#define LEDS ((volatile unsigned char *) 0x80000000)
+
+void set_leds(unsigned char val)
+{
+  *LEDS = val;
+}
+
+unsigned char get_leds(void)
+{
+  return *LEDS;
+}
---
+++ c_code/leds.h
@@ -0,0 +1,8 @@
+
+#ifndef LEDS_H
+#define LEDS_H
+
+extern void set_leds(unsigned char val);
+extern unsigned char get_leds(void);
+
+#endif
