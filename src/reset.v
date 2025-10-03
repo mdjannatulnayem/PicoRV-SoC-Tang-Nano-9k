2091b828bd0b42dd7fb7d4c32c2f58847f29d87b
Md. Jannatul Nayem <nayemalimran106@gmail.com>
2025-10-04 00:05:23 +0600

    peripherals

---
+++ src/reset.v
@@ -0,0 +1,20 @@
+
+module reset_control
+(
+ input wire  clk,
+ input wire  reset_button_n,
+ output wire reset_n
+);
+   reg [5:0] reset_count = 0;
+
+   // picorv32 must see a reset_n rising edge so hold it active
+   // until a count completes.
+   assign reset_n = &reset_count;
+
+   always @(posedge clk)
+     if (reset_button_n)
+       reset_count <= reset_count + !reset_n;
+     else
+       reset_count <= 'b0;
+
+endmodule
